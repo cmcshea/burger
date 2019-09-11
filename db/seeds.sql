5a37435fc4e70b65ed3f9767d8e70593605f5ddf
cmcshea <cory.mcshea@gmail.com>
2019-09-10 22:53:35 -0400

    addind seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Shack Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Beyond Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey Burger", true);
