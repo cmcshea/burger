000943f4be9a5d9ac8a2ea8594bd1749e8e50f1d
cmcshea <cory.mcshea@gmail.com>
2019-09-10 22:21:43 -0400

    building db

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,13 @@
+-- create burger database
 
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- create the table burgers
+
+CREATE TABLE burgers (
+    id int AUTO_INCREMENT,
+    burger_name varchar(60) NOT NULL,
+    devoured BOOLEAN,
+    PRIMARY KEY(id)
+);
